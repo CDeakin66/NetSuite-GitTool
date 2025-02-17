30848cc1b69b6c21761239019d448a478c7b0b16
GitHub Action <action@github.com>
2025-02-17 19:19:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/cdeakin66/netsuitegittool/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/cdeakin66/netsuitegittool/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.cdeakin66.netsuitegittool.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
